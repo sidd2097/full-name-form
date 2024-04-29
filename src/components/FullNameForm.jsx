b419fc289018ba11cd1a254e2408d3efea08f6be
Sidd <siddheshposam20@gmail.com>
2024-04-29 20:29:12 +0530

    add error message

--- src/components/FullNameForm.jsx
+++ src/components/FullNameForm.jsx
@@ -5,24 +5,29 @@ const FullNameForm = ()=> {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [fullName, setFullName] = useState('');
-    const [isFormValid, setIsFormValid] = useState(false);
+    // const [isFormValid, setIsFormValid] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
     
     const handleSubmit = (e) => {
         e.preventDefault();
         if(firstName && lastName) {
             const fullName = `${firstName} ${lastName}`;
             setFullName(fullName);
+            setErrorMessage('');
+        } else {
+            setFullName('');
+            setErrorMessage('Please fill out both fields')
         }
     };
 
     const handleFirstNameChange = (event) => {
         setFirstName(event.target.value);
-        setIsFormValid(event.target.value && lastName);
+        // setIsFormValid(event.target.value && lastName);
     };
     
     const handleLastNameChange = (event) => {
         setLastName(event.target.value);
-        setIsFormValid(event.target.value && firstName);
+        // setIsFormValid(event.target.value && firstName);
     };
 
     return (
@@ -49,6 +54,7 @@ const FullNameForm = ()=> {
                 />
                 </div>
                 <button type='submit' >Submit</button>
+                {errorMessage && <p className='error-message'>{errorMessage}</p>}
             </form>
             {fullName && <p>Full Name: {fullName}</p>}
         </div>
